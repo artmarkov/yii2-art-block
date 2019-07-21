b0f80da8b8bae7e46e215f7a393fd55dd561ada8
artmarkov <artmarkov@mail.ru>
2019-07-21 21:41:05 +0300

    refactor theme

--- views/default/create.php
+++ views/default/create.php
@@ -10,6 +10,10 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('art/block', 'HTML Blocks'),
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="block-create">
-    <h3 class="lte-hide-title"><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="page-title"><?=  Html::encode($this->title) ?></h3>            
+        </div>
+    </div>
     <?= $this->render('_form', compact('model')) ?>
 </div>
