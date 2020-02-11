d280785a8fcfd5ec1929d88b398c4ed335289902
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-11 11:59:37 +0300

    Update _form.php

--- views/default/_form.php
+++ views/default/_form.php
@@ -14,95 +14,52 @@ use artsoft\widgets\LanguagePills;
 
     <?php
     $form = ActiveForm::begin([
-                'id' => 'block-form',
-                'validateOnBlur' => false,
-            ])
+        'id' => 'block-form',
+        'validateOnBlur' => false,
+    ])
     ?>
 
-    <div class="row">
-        <div class="col-md-8">
-
-            <div class="panel panel-default">
-                <div class="panel-body">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-12">
 
                     <?php if ($model->isMultilingual()): ?>
                         <?= LanguagePills::widget() ?>
                     <?php endif; ?>
 
                     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-                    
+
                     <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
-                    
+
                     <?= $form->field($model, 'created_by')->dropDownList(User::getUsersList()) ?>
 
                     <?= $form->field($model, 'content')->widget(trntv\aceeditor\AceEditor::class,
-                            [
-                                'mode' => 'html',
-                                'theme' => 'sqlserver', //chrome,clouds,clouds_midnight,cobalt,crimson_editor,dawn,dracula,dreamweaver,eclipse,iplastic
-                                                        //merbivore,merbivore_soft,sqlserver,terminal,tomorrow_night,twilight,xcode
-                            ]) ?>
+                        [
+                            'mode' => 'html',
+                            'theme' => 'sqlserver', //chrome,clouds,clouds_midnight,cobalt,crimson_editor,dawn,dracula,dreamweaver,eclipse,iplastic
+                            //merbivore,merbivore_soft,sqlserver,terminal,tomorrow_night,twilight,xcode
+                        ]) ?>
 
                 </div>
             </div>
         </div>
+        <div class="panel-footer">
+            <div class="form-group">
+                <?= Html::a(Yii::t('art', 'Go to list'), ['/block/default/index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
 
-        <div class="col-md-4">
-
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-                        <div class="form-group clearfix">
-                          <label class="control-label" style="float: left; padding-right: 5px;">
-                             <?= $model->attributeLabels()['id'] ?>: 
-                          </label>
-                               <span><?= $model->id ?></span>
-                        </div>
-                        
-                        <?php if (!$model->isNewRecord): ?>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['created_at'] ?> :
-                                </label>
-                                <span><?= $model->createdDatetime ?></span>
-                            </div>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['updated_at'] ?> :
-                                </label>
-                                <span><?= $model->updatedDatetime ?></span>
-                            </div>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['updated_by'] ?> :
-                                </label>
-                                <span><?= $model->updatedBy->username ?></span>
-                            </div>
-
-                        <?php endif; ?>
-
-                        <div class="form-group">                            
-                            
-                            <?php if ($model->isNewRecord): ?>
-                                <?= Html::submitButton(Yii::t('art', 'Create'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Cancel'), ['/block/default/index'], ['class' => 'btn btn-default']) ?>
-                            <?php else: ?>
-                                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-                                <?=
-                                Html::a(Yii::t('art', 'Delete'), ['/block/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-danger',
-                                    'data' => [
-                                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'method' => 'post',
-                                    ],
-                                ])
-                                ?>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
+                <?php if (!$model->isNewRecord): ?>
+                    <?=
+                    Html::a(Yii::t('art', 'Delete'), ['/block/default/delete', 'id' => $model->id], [
+                        'class' => 'btn btn-danger',
+                        'data' => [
+                            'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'method' => 'post',
+                        ],
+                    ])
+                    ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
